bd6916a590a4ddd8712f7324f69f6758c8daa28d
sanjay kumar sahu <sanjaykumarsahu@gmail.com>
2024-04-07 09:07:14 +0530

    first commit

---
+++ rough karya/java.js
@@ -0,0 +1,13 @@
+function addtwo(a,b){
+    return a+b;
+}
+
+console.log(addtwo(487,567));
+
+let student={
+    sanjay:'coding',
+    nitin:'gandva',
+    anshu:'boda'
+}
+
+console.log(student['anshu']);
---
+++ rough/rough.js
@@ -0,0 +1,11 @@
+
+const prompt=require('prompt-sync')();
+let a=prompt("hey please enter your age");
+a = 30 // Prompt the user to enter their age
+if(a<18){
+    alert("nabalic");
+
+}
+else{
+    alert("balic");
+}
